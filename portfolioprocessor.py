ac1fcbc27a9ef5b0ac7e3ad648e0bafd503b85a5
nascitur <uvajosh@yahoo.com>
2015-04-13 22:44:23 -0600

    Added 2D function for processing lists

--- portfolioprocessor.py
+++ portfolioprocessor.py
@@ -23,58 +23,82 @@ def getthefile(default_filename):
 	stringified = mmap.mmap(openfile.fileno(), 0, access=mmap.ACCESS_READ)
 	return stringified
 
-
-# get_item_list attempts to generically return list of items from the xml
+# get_item_list returns list of items from the xml
+# by "items" I mean initiatives, releases, teams, etc
 #
-# the item_string is the file snippet
-# the item_xml_name marks the start of the "Collection" in the xml
-# the xml_before is any tag that can be used to find the item start
+# the item_string is the relevant file snippet with the item list
+# the item_xml_name marks the start of the item "Collection" in the xml
+# the xml_before is any tag that can be used to find each specific item start
 # the pad is the character distance from the xml_before tag to the text you want
 # same for the xml_end
 
-def get_item_list(item_string,item_xml_name,item_xml_elem,xml_before,pad_before,xml_after,pad_after):
+def get_item_list(
+	item_string,
+	item_xml_name,
+	item_xml_elem,
+	xml_before,
+	pad_before,
+	xml_after,
+	pad_after):
 	item_startpos = item_string.find('<' + item_xml_name + 'Collection>')
 	item_endpos = item_string.find('</' + item_xml_name + 'Collection>')
 	item_string = item_string[item_startpos:item_endpos]
-	listindex = 0
 	item_list = []
+	i = 0
 	while item_string.find(item_xml_elem) != -1:
-		thisitem_start = item_string.find(item_xml_elem) + len(item_xml_elem)
-		item_string = item_string[thisitem_start:]
-		thisitem_start = item_string.find(xml_before) + pad_before
-		thisitem_end = item_string.find(xml_after) - pad_after
-		item_list.append(listindex)
-		item_list[listindex] = item_string[thisitem_start:thisitem_end]
-		print item_list[listindex]
-		item_string = item_string[thisitem_end + len(item_xml_name):]
-		listindex = listindex + 1
+		item_start = item_string.find(item_xml_elem) + len(item_xml_elem)
+		item_string = item_string[item_start:]
+		item_start = item_string.find(xml_before) + pad_before
+		item_end = item_string.find(xml_after) - pad_after
+		item_list.append(i)
+		item_list[i] = item_string[item_start:item_end]
+		print item_list[i]
+		item_string = item_string[item_end + len(item_xml_name):]
+		i = i + 1
 
-# takes file string as input and returns a list with themes as output
-# assumes each theme is theme id = index of returned list
+def get_item_list_2D(
+	item_string,
+	item_xml_name,
+	item_xml_elem,
+	xml_before,
+	pad_before,
+	xml_after,
+	pad_after,
+	xml_2D_elem):
+	list_startpos = item_string.find('<' + item_xml_name + 'Collection>')
+	list_endpos = item_string.find('</' + item_xml_name + 'Collection>')
+	item_string = item_string[list_startpos:list_endpos]
+	item_list = []
+	item_list.append([])
+	item_list.append([])
+	i = 0
+	j = 0
+	while item_string.find(item_xml_elem) != -1:
+		item_start = item_string.find(item_xml_elem) + len(item_xml_elem)
+		item_string = item_string[item_start:]
+		firstindex_loc = item_string.find(xml_2D_elem) + len(xml_2D_elem) + 2
+		last_i = i
+		i = int(item_string[firstindex_loc:firstindex_loc+1]) - 1
+		if last_i != i: j = 0
+		item_start = item_string.find(xml_before) + pad_before
+		item_end = item_string.find(xml_after) - pad_after
+		print i
+		print j
+		item_list[i].append(j)
+		item_list[i][j] = item_string[item_start:item_end]
+		print item_list[i][j]
+		item_string = item_string[item_end + len(item_xml_name):]
+		j = j + 1
 
-def getthemelist(themestring):
-	themestartpos = themestring.find('<themeCollection>')
-	themeendpos = themestring.find('</themeCollection>')
-	themestring = themestring[themestartpos:themeendpos]
-	listindex = 0
-	themelist = []
-	while themestring.find('<title>') != -1:
-		thisthemestart = themestring.find('<title>') + 16
-		thisthemeend = themestring.find('</title>') - 3
-		themelist.append(listindex)
-		themelist[listindex] = themestring[thisthemestart:thisthemeend]
-		print themelist[listindex]
-		themestring = themestring[thisthemeend + 11:]
-		listindex = listindex + 1
 
-
-
+# Get all our lists
 
 filestring = getthefile('export.xml')
 theme_list = get_item_list(filestring,'theme','<theme ','<title>',16,'</title>',3)
 stream_list = get_item_list(filestring,'stream','<stream ','<title>',16,'</title>',3)
 team_list =  get_item_list(filestring,'team','<team ','<title>',16,'</title>',3)
 initiatives_list = get_item_list(filestring,'workItem','plan-initiatives-1','<title>',16,'</title>',3)
+releases_list = get_item_list_2D(filestring,'release','<release ','<title>',16,'</title>',3,'aostream')
 
 
 
